bf9e6f3a804da1a289f0a01199462521c6460474
vladosara <157279935+vladosara@users.noreply.github.com>
2024-01-21 11:08:08 +0100

    Add files via upload

---
+++ Index.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="WebApplication1.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>PROJEKAT!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="Index.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
+</head>
+<body>
+ 
+    <form id="form1" runat="server">
+        <div id="drzava">
+            <h1>PRITISNI DUGME ZA 1 OD 1001 RAZLOGA</h1>
+        </div>
+        <video autoplay="autoplay" loop="loop" muted="muted" playsinline="playsinline" class="vidoo">
+            <source src="Images/Untitled video - Made with Clipchamp (1).mp4" type="video/mp4" />
+        </video>
+        <div id="dvojika">
+
+
+<asp:LinkButton ID="LinkButton1" runat="server" OnClick="LinkButton1_Click">
+     <i class="fas fa-play"></i>
+</asp:LinkButton>
+            
+        </div>
+        <div id="odgovor" runat="server"> </div>
+    </form>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        static string connString = ConfigurationManager.ConnectionStrings["KonekcioniString"].ConnectionString;
+        SqlConnection conn = new SqlConnection(connString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            HtmlGenericControl div = new HtmlGenericControl("div");
+            div.Attributes["class"] = "odgovor1";
+            Label naslov = new Label();
+            Label naslov2 = new Label();
+            naslov.Attributes["class"] = "blok";
+            naslov2.Attributes["class"] = "blok";
+            string tekst = divKreiranje();
+            naslov.Text = tekst;
+            naslov2.Text = "Razlog zbog kojeg te volim:";
+            div.Controls.Add(naslov2);
+            div.Controls.Add(naslov);
+
+
+            HtmlGenericControl odgovor = FindControl("odgovor") as HtmlGenericControl;
+            if (odgovor != null)
+            {
+                odgovor.Controls.Add(div);
+            }
+
+        }
+
+
+
+        public string divKreiranje()
+        {
+            string tekst = "123"; // Declare a string variable to store the value
+
+            conn.Open();
+            try
+            {
+                string naredba = "SELECT TOP 1 * FROM PORUKE ORDER BY NEWID();";
+                SqlCommand komanda = new SqlCommand(naredba, conn);
+                SqlDataReader citac = komanda.ExecuteReader();
+
+                if (citac.HasRows)
+                {
+                    citac.Read();
+                    // Assuming "TEXT" is the second column (index 1)
+                    tekst = citac.GetString(1);
+                    return tekst;
+                }
+                else
+                {
+                    // Handle the case where no data is found
+                    tekst = "No data found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log or handle the exception more gracefully
+                tekst = "Error: " + ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return tekst;
+        }
+
+    }
+}
